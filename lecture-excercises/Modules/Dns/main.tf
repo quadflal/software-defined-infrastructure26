7ef12863432c3fa62788d557b45effe6d95283b9
quadflal <alexquadf@gmail.com>
2026-07-17 22:23:17 +0200

    Update DNS A record configuration to use sets and increase TTL

--- lecture-excercises/Modules/Dns/main.tf
+++ lecture-excercises/Modules/Dns/main.tf
@@ -1,14 +1,14 @@
 resource "dns_a_record_set" "helloRecord" {
   zone = "${var.dns_zone}." # The dot matters!
   name = var.server_name
-  addresses = var.server_ip
-  ttl = 10
+  addresses = toset([var.server_ip])
+  ttl = 300
 }
 
 resource "dns_a_record_set" "alias" {
   for_each  = toset(distinct(var.server_aliases))
   zone      = "${var.dns_zone}."  # The dot matters!
   name      = each.value
-  addresses = var.server_ip
-  ttl       = 10
+  addresses = toset([var.server_ip])
+  ttl       = 300
 }
